a6762efe4796b3537eee71d8021fcac5dc0001a3
Antonio Queb <antonioqueb@alphaqueb.online>
2024-10-26 18:27:34 -0600

    Up

--- models/extended_mrp_production.py
+++ models/extended_mrp_production.py
@@ -1,4 +1,4 @@
-# file: models/extended_mrp_production.py
+# -*- coding: utf-8 -*-
 
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
@@ -8,6 +8,11 @@ class ExtendedMrpProduction(models.Model):
 
     stage_id = fields.Many2one('mrp.production.stage', string='Etapa de Producción', tracking=True)
     stage_history_ids = fields.One2many('mrp.production.stage.history', 'production_id', string='Historial de Etapas')
+    octagon_runs = fields.Integer(string='Corridas de Octágono')
+    cuts_per_shift = fields.Integer(string='Cortes por Turno')
+    glued_reticles = fields.Integer(string='Retículas Pegadas')
+    linear_meters = fields.Float(string='Metros Lineales')
+    finished_pallets = fields.Integer(string='Tarimas Terminadas')
 
     @api.model
     def create(self, vals):
@@ -36,6 +41,12 @@ class ExtendedMrpProduction(models.Model):
             'user_id': self.env.user.id,
         })
 
+    def action_confirm(self):
+        for production in self:
+            if not production.stage_id:
+                raise UserError(_("Por favor, establezca una etapa de producción antes de confirmar."))
+        return super(ExtendedMrpProduction, self).action_confirm()
+
     def action_next_stage(self):
         for production in self:
             next_stage = self.env['mrp.production.stage'].search([
@@ -45,3 +56,9 @@ class ExtendedMrpProduction(models.Model):
                 production.write({'stage_id': next_stage.id})
             else:
                 raise UserError(_("Esta es la etapa final de producción."))
+
+    def _generate_moves(self):
+        moves = super(ExtendedMrpProduction, self)._generate_moves()
+        for move in moves:
+            move.production_stage_id = self.stage_id.id
+        return moves
